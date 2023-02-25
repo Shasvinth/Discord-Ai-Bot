54781dd87f8f66f46dd652289b985517013b8cb2
Shasvinth Srikanth <shasvinthsrikanth13@gmail.com>
2023-02-25 15:39:30 +0530

    first commit

---
+++ ai.js
@@ -0,0 +1,22 @@
+//In ai.js
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: "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",
+});const openai = new OpenAIApi(configuration);
+async function ask(prompt) {
+    const response = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+    const answer = response.data.choices[0].text;
+    return answer;
+}
+//Export the "ask" function
+module.exports = {
+    ask,
+};
